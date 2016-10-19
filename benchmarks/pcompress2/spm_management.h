09fa48f155f832dabc5c319af070849e321414ab
DongDongJu <commisori28@gmail.com>
2016-10-18 17:06:37 -0700

    fix stat function

---
+++ benchmarks/pcompress2/spm_management.h
@@ -0,0 +1,15 @@
+#define STACK_BASE __builtin_frame_address(0)
+
+#define TROI_main
+#define TROI_compress
+
+#define stack_func_main
+#define stack_func_main_size 16
+#define stack_func_compress
+#define stack_func_compress_size 1064
+
+#define heap_array_in
+#define heap_array_bw
+#define heap_array_rot
+#define heap_array_rle
+#define heap_array_ari
